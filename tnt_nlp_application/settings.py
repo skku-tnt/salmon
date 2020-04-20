09a1962e706be44450c619e45fb94e6d36dd5c37
ruthetum <ruthetum@gmail.com>
2020-04-20 20:07:17 +0900

    settings.py 설정

--- tnt_nlp_application/settings.py
+++ tnt_nlp_application/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '_wl*0h=cnikjt0tafnl$+@5+%svpk^sxj9q4mm=y!wus+ejda('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'applications.apps.ApplicationsConfig',
 ]
 
 MIDDLEWARE = [
@@ -103,18 +104,22 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-Kr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Seoul/Asia'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/uplods/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
