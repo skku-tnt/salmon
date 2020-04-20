b7dee454f04376e9667c6e2e05f33612c6fec8e4
ruthetum <ruthetum@gmail.com>
2020-04-20 20:18:08 +0900

    application-urls.py 생성

---
+++ applications/urls.py
@@ -0,0 +1,8 @@
+from django.conf import settings
+from django.conf.urls import url, include
+from django.conf.urls.static import static
+from django.urls import path
+from . import views
+
+urlpatterns = [
+]
