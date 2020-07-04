6e09601eee075795560eb85adfa9a244e553ab5f
Jungmin <ljm9748@naver.com>
2020-07-05 02:27:47 +0900

    Update cornell
    표형식으로 변경

--- applications/models.py
+++ applications/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+  
 
-# Create your models here.
+class Mynote(models.Model):
+    auther     = models.CharField('작성자', max_length=16, null=False)
+    class_id = models.CharField('강의아이디', max_length=16, null=False)
+    title      = models.CharField('제목', max_length=126, null=False)
+    content    = models.TextField('내용', null=False)
+    keyword    = models.TextField('키워드', null=False)
+    summary    = models.TextField('요악', null=False)
+
