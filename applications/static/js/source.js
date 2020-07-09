6506b18733f9ea11ad2d3a48d727a41c38a19a81
Heedong Kim <ruthetum@gmail.com>
2020-07-09 17:19:33 +0900

    Quiz 띄우기

---
+++ applications/static/js/source.js
@@ -0,0 +1,58 @@
+window.onload = function () {
+    setInterval(loadQuiz, 100);
+};
+
+function loadQuiz() {
+    var player = document.getElementById('myVideo');
+    if (player.currentTime >= 20 && player.currentTime < 20.1) {
+        player.pause();
+
+        if (player.webkitDisplayingFullscreen) {
+            document.webkitCancelFullScreen();
+        }
+        var quizArea = document.getElementById('quizArea');
+        quizArea.style.display = 'block';
+    }
+}
+
+function moveTime(timestamp) {
+    if (timestamp === 'quiz1'){
+        var time = 20;
+    } else if (timestamp === 'quiz2') {
+        var time = 41;
+    }
+    var myVideo = document.getElementById("myVideo");
+    myVideo.currentTime = time;
+    myVideo.play();
+}
+
+function scriptToggle() {
+    var scBtn = document.getElementById('scriptBtn')
+    var sc = document.getElementById('scriptValue').style;
+
+    if (sc.display === "none") {
+        sc.display = "block";
+        scBtn.innerText = "스크립트 닫기";
+    } else {
+        sc.display = "none";
+        scBtn.innerText = "스크립트 열기";
+    }
+}
+
+function questionToggle(quesName) {
+    if (quesName === 'quiz1') {
+        var q = document.getElementById('quizDiv1').style;
+        var a = document.getElementById('ansDiv1').style;
+    } else if (quesName === 'quiz2') {
+        var q = document.getElementById('quizDiv2').style;
+        var a = document.getElementById('ansDiv2').style;
+    }
+
+    if (q.display === 'block') {
+        q.display = 'none';
+        a.display = 'block';
+    } else {
+        q.display = 'block';
+        a.display = 'none';
+    }
+}
