f560918fdb334da62e86100d4c5132b093d59cab
Heedong Kim <ruthetum@gmail.com>
2020-07-19 17:50:24 +0900

    source.js 수정

--- applications/static/js/source.js
+++ applications/static/js/source.js
@@ -1,41 +1,12 @@
-function moveTime(timestamp) {
-    if (timestamp === 'quiz1'){
-        var time = 20;
-    } else if (timestamp === 'quiz2') {
-        var time = 41;
-    }
-    var myVideo = document.getElementById("myVideo");
-    myVideo.currentTime = time;
-    myVideo.play();
-}
-
-function scriptToggle() {
-    var scBtn = document.getElementById('scriptBtn')
-    var sc = document.getElementById('scriptValue').style;
-
-    if (sc.display === "none") {
-        sc.display = "block";
-        scBtn.innerText = "스크립트 닫기";
-    } else {
-        sc.display = "none";
-        scBtn.innerText = "스크립트 열기";
-    }
-}
-
 function questionToggle(quesName) {
     if (quesName === 'quiz1') {
-        var q = document.getElementById('quizDiv1').style;
-        var a = document.getElementById('ansDiv1').style;
+        var a = document.getElementById('ans1');
+        a.innerText = '정답 : 종교개혁';
     } else if (quesName === 'quiz2') {
-        var q = document.getElementById('quizDiv2').style;
-        var a = document.getElementById('ansDiv2').style;
-    }
-
-    if (q.display === 'block') {
-        q.display = 'none';
-        a.display = 'block';
-    } else {
-        q.display = 'block';
-        a.display = 'none';
+        var a = document.getElementBㅉyId('ans2');
+        a.innerText = '정답 : 로마 카톨릭교';
+    } else if (quesName === 'quiz3') {
+        var a = document.getElementById('ans3');
+        a.innerText = '정답 : 츠 빙 글리';
     }
 }
